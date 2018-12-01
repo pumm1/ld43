1a414b6f8c682be8fff6e3b83e5f7b62ecc2e0e4
Tuomo Pelkonen <tuomo.pelkonen@gmail.com>
2018-12-01 20:09:12 +0200

    Camera movement, robot character

---
+++ Assets/Camera/CameraMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	public GameObject followedObject;
+	private Rigidbody2D followedRigidBody;
+	private Camera camera;
+	// Use this for initialization
+	void Start ()
+	{
+		camera = GetComponent<Camera>();
+		followedRigidBody = followedObject.GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		var targetCameraPosition = new Vector3(
+			followedRigidBody.position.x + followedRigidBody.velocity.x*2f, 
+			followedRigidBody.position.y + followedRigidBody.velocity.y*2f + 4f, 
+			- Mathf.Abs(followedRigidBody.velocity.x + followedRigidBody.velocity.y) - 10f );
+		
+		var currentCameraPosition = camera.transform.position;
+		camera.transform.position += (targetCameraPosition - currentCameraPosition) * Time.deltaTime;
+	}
+}
