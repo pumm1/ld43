173bd990db71acc5fcaee49b08df85fa4d145e9f
Tuomo Pelkonen <tuomo.pelkonen@gmail.com>
2018-12-02 13:19:24 +0200

    First level triggers, background

---
+++ Assets/Level/BuildingBlocks/Triggers/DoorLifter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorLifter : MonoBehaviour
+{
+	
+	public GameObject targetObject;
+	private Rigidbody2D targetRigidBody;
+
+	void Start ()
+	{
+		targetRigidBody = targetObject.GetComponent<Rigidbody2D>();
+	}
+	
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag != "Player")
+		{
+			return;
+		}
+		targetRigidBody.gravityScale = -1;
+	}
+
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.tag != "Player")
+		{
+			return;
+		}
+		targetRigidBody.gravityScale = 1;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
