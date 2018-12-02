4ee4afef7524677d96e0d2546297f5a26ee19f01
Tuomo Pelkonen <tuomo.pelkonen@gmail.com>
2018-12-02 23:59:16 +0200

    Added Lava death Added decoration objects

---
+++ Assets/Level/BuildingBlocks/Lava/LavaDeath.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaDeath : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	private void OnTriggerEnter2D(Collider2D collider)
+	{
+		var rigidBody = collider.gameObject.transform.GetComponent<Rigidbody2D>();
+		rigidBody.mass = 10; 
+		rigidBody.drag = 10;
+		rigidBody.gravityScale = 1;
+		rigidBody.angularDrag = 5;
+		if (collider.tag == "Player")
+		{
+			Debug.Log("Player DIED! NOOOOOOOOOOO!");
+			return;
+		}
+	}
+}
